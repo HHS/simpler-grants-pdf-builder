78195bbd508e7a9c3d6d5f77ad3cd9afdaa4c633
Paul Craig <paul@pcraig3.ca>
2023-12-29 00:32:30 -0500

    Remove urlize markdown extension that was resulting in empty links
    It seems like urlize will try and create links out of urls that are
    just lying around, but if the link text of a markdown link is itself
    a link, it gets confused.
    
    Removed this and the problem went away (was seeing this with endnotes)
    
    Here is an example link it was messing up:
    [https://www.cdc.gov/nchs/index.htm](https://www.google.com/url?q=https://www.cdc.gov/nchs/index.htm&sa=D&source=editors&ust=1699482269265291&usg=AOvVaw1Hv5wylZ_Zl6pBTwhzaS_3)

--- bloom_nofos/bloom_nofos/settings.py
+++ bloom_nofos/bloom_nofos/settings.py
@@ -268,7 +268,7 @@ MARTOR_MARKDOWN_EXTENSIONS = [
     "markdown.extensions.smarty",
     "markdown.extensions.fenced_code",
     # Custom markdown extensions.
-    "martor.extensions.urlize",
+    # "martor.extensions.urlize",
     "martor.extensions.del_ins",  # ~~strikethrough~~ and ++underscores++
     "martor.extensions.mention",  # to parse markdown mention
     # "martor.extensions.emoji",  # to parse markdown emoji
