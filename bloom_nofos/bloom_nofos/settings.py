80f321a2e5a65bbfc853a88804c4263f743d9d27
Paul Craig <paul@pcraig3.ca>
2024-01-04 09:59:05 -0500

    Update Django version
    Can't update for 5 yet since "django-project-version" doesn't let me.
    But that's fine, I can wait.
    
    I also removed come redundant config from the settings file.

--- bloom_nofos/bloom_nofos/settings.py
+++ bloom_nofos/bloom_nofos/settings.py
@@ -155,24 +155,6 @@ if DEBUG:
     print("===== DATABASES")
     print(DATABASES)
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -210,7 +192,9 @@ LOGIN_URL = "/login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-# Password config: https://docs.djangoproject.com/en/4.2/topics/auth/passwords/#enabling-password-validation
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
--- bloom_nofos/bloom_nofos/tests.py
+++ bloom_nofos/bloom_nofos/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.conf import settings
 from django.urls import reverse
 
 
