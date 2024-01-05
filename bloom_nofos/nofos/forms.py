785914a4c2dbb8b9bbd12b5005e0ae711711fc2f
Paul Craig <paul@pcraig3.ca>
2024-01-05 18:13:35 -0500

    Refactor the form classes
    A lot of our forms are basically the same, so create a function that
    creates form classes for us.

--- bloom_nofos/nofos/forms.py
+++ bloom_nofos/nofos/forms.py
@@ -3,59 +3,43 @@ from .models import Nofo, Subsection
 from martor.fields import MartorFormField
 
 
-class BaseNofoRequiredFieldForm(forms.ModelForm):
+def create_nofo_form_class(field_arr, not_required_field_labels=None):
     """
-    Create a BaseNofoRequiredFieldForm that sets required=True for fields that are not specified in "not_required_field_labels"
+    Factory function to create Nofo form classes dynamically.
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            if not (
-                hasattr(self, "not_required_field_labels")
-                and field.label in self.not_required_field_labels
-            ):
-                field.required = True
+    class _NofoForm(forms.ModelForm):
+        class Meta:
+            model = Nofo
+            fields = field_arr
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field in self.fields.values():
+                if (
+                    not_required_field_labels
+                    and field.label in not_required_field_labels
+                ):
+                    field.required = False
+                else:
+                    field.required = True
+
+    return _NofoForm
 
 
-class NofoNameForm(BaseNofoRequiredFieldForm):
-    not_required_field_labels = ["Short name"]
+# Creating form classes
+NofoNameForm = create_nofo_form_class(
+    ["title", "short_name"], not_required_field_labels=["Short name"]
+)
 
-    class Meta:
-        model = Nofo
-        fields = ["title", "short_name"]
-
-
-class NofoCoachForm(BaseNofoRequiredFieldForm):
-    class Meta:
-        model = Nofo
-        fields = ["coach"]
+NofoCoachForm = create_nofo_form_class(["coach"])
+NofoNumberForm = create_nofo_form_class(["number"])
+NofoApplicationDeadlineForm = create_nofo_form_class(["application_deadline"])
+NofoTaglineForm = create_nofo_form_class(["tagline"])
+NofoThemeForm = create_nofo_form_class(["theme"])
 
 
-class NofoNumberForm(BaseNofoRequiredFieldForm):
-    class Meta:
-        model = Nofo
-        fields = ["number"]
-
-
-class NofoApplicationDeadlineForm(BaseNofoRequiredFieldForm):
-    class Meta:
-        model = Nofo
-        fields = ["application_deadline"]
-
-
-class NofoTaglineForm(BaseNofoRequiredFieldForm):
-    class Meta:
-        model = Nofo
-        fields = ["tagline"]
-
-
-class NofoThemeForm(BaseNofoRequiredFieldForm):
-    class Meta:
-        model = Nofo
-        fields = ["theme"]
-
-
+# this one needs a custom field and a custom widget so don't use the factory function
 class SubsectionForm(forms.ModelForm):
     body = MartorFormField(required=False)
 
