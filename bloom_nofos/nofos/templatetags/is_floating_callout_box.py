953d04edb6606ea916586c348fe37fa409180c6e
Paul Craig <paul@pcraig3.ca>
2024-01-23 17:04:49 -0500

    Add a separate column for callout boxes in portrait mode
    Basically, callout boxes will go into the top right for portrait mode
    PDFs, or they will stay inline.
    
    Right now it is based on the heading title but this will only get us so far.

---
+++ bloom_nofos/nofos/templatetags/is_floating_callout_box.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter()
+def is_floating_callout_box(subsection):
+    # this is not a good idea long-term but it works for now
+    floating_subsection_strings = [
+        "Key facts",
+        "Key dates",
+        "Questions?",
+        "Have questions?",
+    ]
+
+    if subsection.name:
+        return subsection.name.strip() in floating_subsection_strings
+
+    for subsection_string in floating_subsection_strings:
+        if subsection.body.strip().startswith(subsection_string):
+            return True
+
+    return False
