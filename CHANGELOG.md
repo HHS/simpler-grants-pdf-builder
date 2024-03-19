9872fcad83d5485adc6b07fd35a6303a9ab853c3
Paul Craig <paul@pcraig3.ca>
2024-03-19 10:39:29 -0400

    Accommodate for HTML in strings for our right-col callout boxes
    If there was HTML in the string (like a link), then it would
    not move it and leave it in the strong tag, not what we wanted.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ The format is based on Keep a Changelog, and this project adheres to Semantic Ve
 
 ### Fixed
 
+- Handle HTML in strings for the wrap_text_before_colon_in_strong function
 - Remove static_icon function and all static images associated with it
 
 ## [1.15.0] - 2023-03-09
--- bloom_nofos/nofos/templatetags/callout_box_contents.py
+++ bloom_nofos/nofos/templatetags/callout_box_contents.py
@@ -16,16 +16,16 @@ def wrap_text_before_colon_in_strong(p, soup):
     for content in p.contents[:]:
         if found_colon:
             span_tag.append(content.extract())
-
-        if ":" in content:
-            before_colon, after_colon = content.split(":", 1)
-            strong_tag.append(before_colon + ":")
-            # Replace the original content with what's after the colon
-            span_tag.append(after_colon)
-            found_colon = True  # Mark colon as found
         else:
-            # Move content to strong tag if colon not yet found
-            strong_tag.append(content.extract())
+            if isinstance(content, str) and ":" in content:
+                before_colon, after_colon = content.split(":", 1)
+                strong_tag.append(before_colon + ":")
+                # Replace the original content with what's after the colon
+                span_tag.append(after_colon)
+                found_colon = True  # Mark colon as found
+            else:
+                # Move content to strong tag if colon not yet found
+                strong_tag.append(content.extract())
 
     # Insert the strong tag at the beginning of the paragraph
     p.clear()
