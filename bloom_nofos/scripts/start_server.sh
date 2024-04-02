cb18a7c3b09c9191f7d84c2fade9d04a1aece58b
Paul Craig <paul@pcraig3.ca>
2024-04-02 11:02:59 -0400

    Raise the timeout time to 89 seconds

--- bloom_nofos/scripts/start_server.sh
+++ bloom_nofos/scripts/start_server.sh
@@ -1,1 +1,1 @@
-/root/.local/bin/poetry run gunicorn --workers 8 --timeout 59 --chdir bloom_nofos --bind 0.0.0.0:$PORT bloom_nofos.wsgi:application
+/root/.local/bin/poetry run gunicorn --workers 8 --timeout 89 --chdir bloom_nofos --bind 0.0.0.0:$PORT bloom_nofos.wsgi:application
