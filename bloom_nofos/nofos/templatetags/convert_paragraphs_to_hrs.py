8be4c150b5c1529dbbdf789fbc844a071c731efd
Paul Craig <paul@pcraig3.ca>
2024-04-10 16:22:24 -0400

    Rebuild function for the page-break lines so that they are searchable

---
+++ bloom_nofos/nofos/templatetags/convert_paragraphs_to_hrs.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from django import template
+from django.utils.safestring import mark_safe
+
+from .utils import convert_paragraph_to_searchable_hr
+
+register = template.Library()
+
+
+@register.filter()
+def convert_paragraphs_to_hrs(html_string):
+    soup = BeautifulSoup(html_string, "html.parser")
+    for p in soup.find_all("p", string="page-break-before"):
+        convert_paragraph_to_searchable_hr(p)
+
+    return mark_safe(str(soup))
