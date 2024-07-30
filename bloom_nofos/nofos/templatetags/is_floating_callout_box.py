e627a7b9e09a61080896c4ed6b835fdeb05a68e7
Paul Craig <paul@pcraig3.ca>
2024-07-30 19:07:05 +0100

    Remove empty "absolute--right-col" wrapper from sections that don't need it
    Not all sections (in fact, most of them of them) will have callout
    boxes that sit along the right-hand column.
    
    However, they would all have an empty container there that wasn't being
    used. Now! They don't!!
    
    Also added some tests for the is_floating_callout_box function.

--- bloom_nofos/nofos/templatetags/is_floating_callout_box.py
+++ bloom_nofos/nofos/templatetags/is_floating_callout_box.py
@@ -1,24 +1,10 @@
 from django import template
 
+from .utils import is_floating_callout_box as is_floating_callout_box_func
+
 register = template.Library()
 
 
 @register.filter()
 def is_floating_callout_box(subsection):
-    # this is not the best idea long-term but it works for now
-    floating_subsection_strings = [
-        "Key facts",
-        "Key dates",
-        "Questions?",
-        "Have questions?",
-        "**Have questions?",
-    ]
-
-    if subsection.name:
-        return subsection.name.strip() in floating_subsection_strings
-
-    for subsection_string in floating_subsection_strings:
-        if subsection.body.strip().startswith(subsection_string):
-            return True
-
-    return False
+    return is_floating_callout_box_func(subsection)
--- bloom_nofos/nofos/templatetags/utils/__init__.py
+++ bloom_nofos/nofos/templatetags/utils/__init__.py
@@ -273,3 +273,23 @@ def format_footnote_ref_html(a_tag):
     else:
         a_tag["id"] = "ftnt_ref{}".format(footnote_number)
         a_tag["href"] = "#ftnt{}".format(footnote_number)
+
+
+def is_floating_callout_box(subsection):
+    # maybe not the best idea long-term but it works for now
+    floating_subsection_strings = [
+        "Key facts",
+        "Key dates",
+        "Questions?",
+        "Have questions?",
+        "**Have questions?",
+    ]
+
+    if subsection.name:
+        return subsection.name.strip() in floating_subsection_strings
+
+    for subsection_string in floating_subsection_strings:
+        if subsection.body.strip().startswith(subsection_string):
+            return True
+
+    return False
