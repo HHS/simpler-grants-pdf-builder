becfcf4bf155f82ca5dd85d5536dfdcc91d3c144
Paul Craig <paul@pcraig3.ca>
2024-10-01 16:27:54 -0400

    Highlight broken links on the nofo_edit page
    Now we can see at a glance which links are broken.

---
+++ bloom_nofos/nofos/templatetags/add_classes_to_links.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from django import template
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter()
+def add_classes_to_broken_links(html_string, broken_links):
+    """
+    Adds "nofo_edit--broken-link" class to links with href matching my broken links array. Also adds a tooltip for broken links.
+
+    Args:
+        html_string (str): The HTML content of a subsection as a string.
+        broken_links (list of dict): A list of broken links dicts returned from "find_broken_links" function nofo.py.
+
+    Example:
+        html = '<p><a href="http://example.com">Visit</a></p>'
+        broken_links = [{'href': 'http://example.com'}]
+        result = add_classes_to_broken_links(html, broken_links)
+        # Output: '<p><a href="http://example.com" class="nofo_edit--broken-link usa-tooltip" data-position="top" title="Broken link">Visit</a></p>'
+    """
+    soup = BeautifulSoup(html_string, "html.parser")
+    link_hrefs = [link["link_href"] for link in broken_links]
+
+    for link in soup.find_all("a", href=True):
+        if link["href"] in link_hrefs:
+            # Add "nofo_edit--broken-link" class to links with a matching href (and tooltip)
+            link["class"] = link.get("class", []) + [
+                "nofo_edit--broken-link",
+                "usa-tooltip",
+            ]
+            link["data-position"] = "top"
+            link["title"] = "Broken link"
+
+    return mark_safe(str(soup))
