1f7026e1b5bd80a133e43cce0aa8f863acbc6778
Paul Craig <paul@pcraig3.ca>
2024-11-19 22:33:22 -0500

    Add class to make smaller text in right col callout boxes
    If there is too much text in the right hand callout boxes, this gets activated.

--- bloom_nofos/nofos/templatetags/get_floating_callout_boxes_from_section.py
+++ bloom_nofos/nofos/templatetags/get_floating_callout_boxes_from_section.py
@@ -12,3 +12,12 @@ def get_floating_callout_boxes_from_section(section):
         for subsection in section.subsections.all().order_by("order")
         if subsection.callout_box and is_floating_callout_box(subsection)
     ]
+
+
+@register.filter()
+def get_combined_wordcount_for_subsections(subsections):
+    word_count = 0
+    for subsection in subsections:
+        word_count += len(subsection.body.split())
+
+    return word_count
