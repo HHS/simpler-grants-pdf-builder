1c56b9cfbb9f91eb07d1e92b866419300af5549b
Paul Craig <paul@pcraig3.ca>
2024-11-22 13:01:39 -0500

    Let's export those links yo
    Pretty simple: added a script that loops through all non-archived NOFOs and will pull all external links. This helps us do analysis on them if we need to for some reason. (In this case, our reason was to manually replace bad links)

---
+++ bloom_nofos/nofos/management/commands/export_links.py
@@ -0,0 +1,97 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from nofos.models import Nofo
+from nofos.nofo import find_external_links
+
+
+class Command(BaseCommand):
+    help = "Extracts external links from NOFO documents and outputs them."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "nofo_id", nargs="?", type=int, help="ID of the NOFO to extract links from"
+        )
+        parser.add_argument(
+            "--all", action="store_true", help="Extract links from all NOFOs"
+        )
+        parser.add_argument(
+            "--output",
+            type=str,
+            default="nofo_links.csv",
+            help="Output file name for the CSV (default: nofo_links.csv)",
+        )
+        parser.add_argument(
+            "--live",
+            type=bool,
+            default=False,
+            help="Specify if links are live",
+        )
+
+    def handle(self, *args, **options):
+        output_file = options["output"]
+        domain = "https://nofo.rodeo" if options["live"] else "http://localhost:8000"
+
+        # Determine which NOFOs to process
+        if options["all"]:
+            nofos = Nofo.objects.all()
+        else:
+            nofo_id = options.get("nofo_id")
+            if nofo_id is None:
+                self.stdout.write(self.style.ERROR("No NOFO ID provided"))
+                return
+            nofos = Nofo.objects.filter(id=nofo_id)
+
+        if not nofos.exists():
+            self.stdout.write(self.style.ERROR("No NOFOs found."))
+            return
+
+        links_count = 0
+
+        # Write the output to a CSV file
+        with open(output_file, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    "nofo_id",
+                    "nofo_number",
+                    "url",
+                    "section_name",
+                    "subsection_name",
+                    "link_to_subsection",
+                ]
+            )
+
+            for nofo in nofos:
+                print("---")
+                print("NOFO: {}".format(nofo.number))
+                links = find_external_links(nofo)
+                print("Links in this NOFO: {}".format(len(links)))
+                links_count += len(links)
+
+                for link in links:
+                    writer.writerow(
+                        [
+                            nofo.id,
+                            nofo.number,
+                            link["url"],
+                            (
+                                link["section"].name if link["section"] else ""
+                            ),  # Section name
+                            (
+                                link["subsection"].name if link["subsection"] else ""
+                            ),  # Subsection name
+                            (
+                                "{}/nofos/{}/section/{}/subsection/{}/edit".format(
+                                    domain,
+                                    nofo.id,
+                                    link["section"].id,
+                                    link["subsection"].id,
+                                )
+                            ),
+                        ]
+                    )
+
+        print("---")
+        print("Total links in all NOFOs: {}".format(links_count))
+        self.stdout.write(self.style.SUCCESS(f"Links extracted to {output_file}"))
