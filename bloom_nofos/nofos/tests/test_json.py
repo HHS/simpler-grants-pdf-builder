79b286c6945b6724cb72e1a35f19da4ec574a7ae
Paul Craig <paul@pcraig3.ca>
2024-11-29 16:16:34 -0500

    Tests to make sure that non-superusers can't see import routes
    There's a test for the GET and the POST route.

--- bloom_nofos/nofos/tests/test_json.py
+++ bloom_nofos/nofos/tests/test_json.py
@@ -125,6 +125,14 @@ class NofoImportJsonViewTest(TestCase):
             force_password_reset=False,
         )
 
+        # Create a regular user
+        self.regular_user = User.objects.create_user(
+            email="regular@groundhog-day.com",
+            password="password",
+            group="bloom",
+            force_password_reset=False,
+        )
+
         self.nofo_empty = "{}"
         self.nofo_no_sections = '{"title": "No sections NOFO","sections": []}'
         self.nofo_no_subsections = '{"title":"NOFO missing subsection","sections":[{"id":595,"nofo":105,"name":"Step 1: Review the Opportunity","order":1,"subsections":[{"id":9175,"section":595,"name":"Basic information","tag":"h3","body":"This is the basic information"}]},{"id":596,"nofo":105,"name":"Step 2: Something else","order":2,"subsections":[]}]}'
@@ -162,6 +170,34 @@ class NofoImportJsonViewTest(TestCase):
         )
 
         return json_data, uploaded_json_file
+
+    def test_regular_user_cannot_get_import_page(self):
+        # Log in as regular user
+        self.client.login(username="regular@groundhog-day.com", password="password")
+
+        response = self.client.get(self.import_url)
+
+        # Assert forbidden response
+        self.assertEqual(response.status_code, 403)
+
+    def test_regular_user_cannot_import_valid_nofo_json_file(self):
+        # Log in as regular user
+        self.client.login(username="regular@groundhog-day.com", password="password")
+
+        _, uploaded_json_file = self._load_json_fixture("cms-u2u-25-001.json")
+        response = self.client.post(
+            self.import_url,
+            {"nofo-import-json": uploaded_json_file},
+        )
+
+        # Assert forbidden response
+        self.assertEqual(response.status_code, 403)
+
+        # Assert that no NOFO with this number exists in the database
+        self.assertFalse(
+            Nofo.objects.filter(number="CMS-2U2-25-001").exists(),
+            "A NOFO object was created when it shouldn't have been.",
+        )
 
     def test_import_empty_json_file(self):
         self.client.login(email="admin@groundhog-day.com", password="superpassword")
