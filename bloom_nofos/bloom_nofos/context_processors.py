6d45d892e2fadfbd8d5e997a716bdb2aabe27113
Paul Craig <paul@pcraig3.ca>
2024-12-02 18:42:18 -0500

    "Live" mode will only last for 5 minutes
    I've switched the variable used for TEST/LIVE mode to be a datetime
    instead of a boolean variable.
    
    With a datetime, we can set a time limit on how long Live mode
    will last. Instead of returning True/False directly, we are
    comparing the timestamp to the current time and returning False if
    it is within the last 5 minutes.
    
    This change requires a bit of database of fiddling otherwise Constance
    throws an error. Big yuck.

--- bloom_nofos/bloom_nofos/context_processors.py
+++ bloom_nofos/bloom_nofos/context_processors.py
@@ -1,9 +1,14 @@
 from constance import config
 from django.conf import settings
 
+from .utils import is_docraptor_test_mode_active
+
 
 def add_docraptor_test_mode(request):
-    return {"DOCRAPTOR_TEST_MODE": getattr(config, "DOCRAPTOR_TEST_MODE")}
+    last_updated = getattr(config, "DOCRAPTOR_TEST_MODE")
+    docraptor_test_mode = is_docraptor_test_mode_active(last_updated)
+
+    return {"DOCRAPTOR_TEST_MODE": docraptor_test_mode}
 
 
 def add_github_sha(request):
