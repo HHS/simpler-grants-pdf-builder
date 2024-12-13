a956957435fcf1d8e6016ecacab9ac34dea3097c
jtmst <josh.troy.mills@gmail.com>
2024-12-13 11:47:31 -0500

    schema derived from model

--- bloom_nofos/nofos/api/schemas.py
+++ bloom_nofos/nofos/api/schemas.py
@@ -1,49 +1,68 @@
-from ninja import Schema
+from ninja import ModelSchema, Schema
 from typing import List, Optional
+from nofos.models import Nofo, Section, Subsection
 
 
-class SubsectionSchema(Schema):
-    name: str
-    html_id: str
-    order: int
-    tag: str
-    body: str
-    callout_box: bool = False
-    html_class: str = ""
+class SubsectionSchema(ModelSchema):
+    class Config:
+        model = Subsection
+        model_fields = [
+            "name",
+            "html_id",
+            "order",
+            "tag",
+            "body",
+            "callout_box",
+            "html_class",
+        ]
+        model_fields_optional = ["html_class"]  # Fields that should have defaults
 
 
-class SectionSchema(Schema):
-    name: str
-    html_id: str
-    order: int
-    has_section_page: bool
+class SectionSchema(ModelSchema):
     subsections: List[SubsectionSchema]
 
+    class Config:
+        model = Section
+        model_fields = ["name", "html_id", "order", "has_section_page"]
 
-class NofoSchema(Schema):
-    title: str
-    filename: str
-    short_name: str
-    number: str
-    opdiv: str
-    agency: str
-    tagline: str
-    application_deadline: str
-    subagency: str = ""
+
+class NofoSchema(ModelSchema):
     sections: List[SectionSchema]
-    # Required fields with defaults
-    theme: str = "portrait-hrsa-blue"
-    cover: str = "nofo--cover-page--medium"
-    icon_style: str = "nofo--icons--border"
-    status: str = "draft"
-    # Optional fields
-    subagency2: str = ""
-    author: str = ""
-    subject: str = ""
-    keywords: str = ""
-    cover_image: str = ""
-    cover_image_alt_text: str = ""
-    inline_css: str = ""
+
+    class Config:
+        model = Nofo
+        model_fields = [
+            "title",
+            "filename",
+            "short_name",
+            "number",
+            "opdiv",
+            "agency",
+            "tagline",
+            "application_deadline",
+            "subagency",
+            "subagency2",
+            "author",
+            "subject",
+            "keywords",
+            "theme",
+            "cover",
+            "icon_style",
+            "status",
+            "cover_image",
+            "cover_image_alt_text",
+            "inline_css",
+        ]
+        model_fields_optional = [
+            "subagency",
+            "subagency2",
+            "author",
+            "subject",
+            "keywords",
+            "cover_image",
+            "cover_image_alt_text",
+            "inline_css",
+        ]
 
 
 class ErrorSchema(Schema):
