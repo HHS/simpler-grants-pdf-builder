415b2225c291f7e2200a2f2635bcd2d641e8a9b1
Paul Craig <paul@pcraig3.ca>
2025-01-07 19:25:13 -0500

    Make sure there is a space between colons and links in callout boxes
    We do a bit of somewhat finnicky HTML processing for the callout boxes
    at the beginning of the NOFO. Since the text is predictable enough,
    we wrap the bit before the colon in a strong tag and then slap
    a span around the rest of it.
    
    So if we start with this:
    
    - <p>Opportunity number: CMS-2U2-25-001</p>
    
    Then the resultant HTML would look like this:
    
    - <p><strong>Opportunity number:<strong><span> CMS-2U2-25-001</span></p>
    
    That's great and all, but when we had a link in there instead of
    a text string, the space between them would disappear, so we would
    literally see text like:
    
    - Key:Link
    
    In order to get it to say, "Key: Link", I've added a space to the
    end of the <strong> tag, and no one's the wiser.
    
    I've also added some tests to this function since it's a little flaky.

--- bloom_nofos/nofos/templatetags/callout_box_contents.py
+++ bloom_nofos/nofos/templatetags/callout_box_contents.py
@@ -4,35 +4,10 @@ from bs4 import BeautifulSoup
 from django import template
 from django.utils.safestring import mark_safe
 
+from .utils import wrap_text_before_colon_in_strong
+
+
 register = template.Library()
-
-
-def wrap_text_before_colon_in_strong(p, soup):
-    # Initialize a flag to track when the colon is found
-    found_colon = False
-    # Create a strong tag
-    strong_tag = soup.new_tag("strong")
-    span_tag = soup.new_tag("span")
-
-    # Iterate over contents, moving elements before the colon to the strong tag
-    for content in p.contents[:]:
-        if found_colon:
-            span_tag.append(content.extract())
-        else:
-            if isinstance(content, str) and ":" in content:
-                before_colon, after_colon = content.split(":", 1)
-                strong_tag.append(before_colon + ":")
-                # Replace the original content with what's after the colon
-                span_tag.append(after_colon)
-                found_colon = True  # Mark colon as found
-            else:
-                # Move content to strong tag if colon not yet found
-                strong_tag.append(content.extract())
-
-    # Insert the strong tag at the beginning of the paragraph
-    p.clear()
-    p.append(strong_tag)
-    p.append(span_tag)
 
 
 @register.filter()
