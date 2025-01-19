ff3cfc0dc48e861b3b869f73d045bf8214d77a51
Paul Craig <paul@pcraig3.ca>
2025-01-19 18:10:08 -0500

    Programmatically create breadcrumb nav
    Previously, this has been hard-coded, but it means that everything gets
    messed up if there is a NOFO with a different number of sections.
    
    Adding this in, similar to what we already have in _icon_macro.html,
    which lets us return breadcrumb labels with reasonable confidence
    because we know how standardized the NOFO sections are.
    
    May have to change the "Learn" one to accomodate for a new template,
    but let's do this for now.
    
    Also: this clears up a TODO that's been in there for like 8 months.

---
+++ bloom_nofos/nofos/templatetags/get_breadcrumb.py
@@ -0,0 +1,19 @@
+from django import template
+
+from .utils import (
+    filter_breadcrumb_sections as _filter_breadcrumb_sections,
+    get_breadcrumb_text,
+)
+
+
+register = template.Library()
+
+
+@register.filter
+def filter_breadcrumb_sections(sections):
+    return _filter_breadcrumb_sections(sections)
+
+
+@register.filter
+def get_breadcrumb(section_name):
+    return get_breadcrumb_text(section_name)
