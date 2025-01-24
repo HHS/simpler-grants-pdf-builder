b359ec53768f0d060b1a513fe48f5d992e37174b
jtmst <josh.troy.mills@gmail.com>
2025-01-24 13:14:45 -0500

    wip

--- bloom_nofos/users/urls.py
+++ bloom_nofos/users/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
@@ -6,16 +5,9 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path(
-        "login",
-        auth_views.LoginView.as_view(template_name="users/login.html"),
-        name="login",
-    ),
-    path(
-        "logout",
-        auth_views.LogoutView.as_view(),
-        name="logout",
-    ),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("auth/callback/", views.callback, name="auth_callback"),
     path(
         "account", login_required(views.BloomUserDetailView.as_view()), name="user_view"
     ),
