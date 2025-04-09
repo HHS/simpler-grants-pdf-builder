345722092dbd2b735276960ee721ce90f8924d67
Paul Craig <paul@pcraig3.ca>
2025-04-09 18:35:13 -0400

    Finally, subclass the Section class as well
    Now everything has changed, amazing.
    
    Let's see how much of the import logic we can recycle.

--- bloom_nofos/guides/admin.py
+++ bloom_nofos/guides/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import ContentGuide
+from .models import ContentGuide, ContentGuideSection, ContentGuideSubsection
+
+
+class ContentGuideSubsectionInline(admin.TabularInline):
+    model = ContentGuideSubsection
+    extra = 1
+
+
+class ContentGuideSectionInline(admin.TabularInline):
+    model = ContentGuideSection
+    extra = 1
+    inlines = [
+        ContentGuideSubsectionInline
+    ]  # Note: Django admin doesn't natively support nested inlines, so this won’t work out of the box unless using a third-party package like django-nested-admin
+    show_change_link = True
 
 
 @admin.register(ContentGuide)
 class ContentGuideAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "filename", "created", "updated")
+    inlines = [ContentGuideSectionInline]
