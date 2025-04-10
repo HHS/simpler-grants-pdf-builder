7892648be729b67ba532fe1a494c0f1e8b37c596
Paul Craig <paul@pcraig3.ca>
2025-04-10 11:14:39 -0400

    Add "Update title" page after importing a content guide

---
+++ bloom_nofos/guides/forms.py
@@ -0,0 +1,3 @@
+from nofos.forms import create_nofo_form_class
+
+ContentGuideTitleForm = create_nofo_form_class(["title"])
--- bloom_nofos/guides/views.py
+++ bloom_nofos/guides/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseBadRequest
 
 
 from nofos.views import BaseNofoImportView
+from nofos.mixins import GroupAccessObjectMixin
 from nofos.nofo import (
     suggest_nofo_title,
     suggest_nofo_opdiv,
@@ -18,6 +19,7 @@ from nofos.utils import create_nofo_audit_event
 
 from guides.models import ContentGuideSection, ContentGuideSubsection
 from guides.guide import create_content_guide
+from guides.forms import ContentGuideTitleForm
 
 
 class ContentGuideListView(ListView):
@@ -57,12 +59,21 @@ class ContentGuideImportView(BaseNofoImportView):
             create_nofo_audit_event(
                 event_type="nofo_import", document=guide, user=request.user
             )
-            return redirect("guides:guide_index")
+            return redirect("guides:guide_edit_title", pk=guide.pk)
 
         except (ValidationError, HeadingValidationError) as e:
             return HttpResponseBadRequest(f"Error creating Content Guide: {e}")
         except Exception as e:
             return HttpResponseBadRequest(f"Error creating Content Guide: {str(e)}")
+
+
+class ContentGuideEditTitleView(GroupAccessObjectMixin, UpdateView):
+    model = ContentGuide
+    form_class = ContentGuideTitleForm
+    template_name = "guides/guide_edit_title.html"
+
+    def get_success_url(self):
+        return reverse_lazy("guides:guide_index")
 
 
 class ContentGuideEditView(UpdateView):
