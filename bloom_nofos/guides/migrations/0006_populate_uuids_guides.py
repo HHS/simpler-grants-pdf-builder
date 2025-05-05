b1aa8d079898926b5b37c9bcf68d13301120d61b
Paul Craig <paul@pcraig3.ca>
2025-05-05 15:47:24 -0400

    Run migration to create new UUIDs for all objects
    During the first migration, all the UUIDs would have been the same
    (eg, all Nofo objects had this UUID: `f99093da-e204-4c5b-9ecd-d1d49f78c1a9`)
    
    So by running this new migration, we assign a unique UUID to all the
    objects.

---
+++ bloom_nofos/guides/migrations/0006_populate_uuids_guides.py
@@ -0,0 +1,22 @@
+# guides/migrations/0006_populate_uuids.py
+from django.db import migrations
+import uuid
+
+
+def regenerate_uuids(apps, schema_editor):
+    for model_name in ["ContentGuide", "ContentGuideSection", "ContentGuideSubsection"]:
+        Model = apps.get_model("guides", model_name)
+        for obj in Model.objects.all():
+            obj.uuid = uuid.uuid4()
+            obj.save(update_fields=["uuid"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("guides", "0005_contentguide_uuid_contentguidesection_uuid_and_more"),
+    ]
+
+    operations = [
+        migrations.RunPython(regenerate_uuids, reverse_code=migrations.RunPython.noop),
+    ]
