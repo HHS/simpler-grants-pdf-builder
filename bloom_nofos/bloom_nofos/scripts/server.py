d7a530983bb6d2470438870c5f4f58cf9d78c3a1
Paul Craig <paul@pcraig3.ca>
2025-05-05 12:03:30 -0400

    Add collectstatic command to Makefile and poetry scripts
    This is something we need to do to boot up the app! So let's add it.

--- bloom_nofos/bloom_nofos/scripts/server.py
+++ bloom_nofos/bloom_nofos/scripts/server.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def main():
@@ -7,7 +8,23 @@ def main():
 
 
 def collectstatic():
-    cmd = ["python", "manage.py", "collectstatic", "--noinput"]
+    # Get last arg (if any)
+    verbosity = sys.argv[1] if len(sys.argv) > 1 else "1"
+
+    # Check if it looks like a digit; fallback to "1"
+    if not verbosity.isdigit():
+        print(f"Warning: invalid verbosity value '{verbosity}', defaulting to 1")
+        verbosity = "1"
+
+    cmd = [
+        "python",
+        "manage.py",
+        "collectstatic",
+        "--noinput",
+        "--verbosity",
+        verbosity,
+    ]
+
     subprocess.run(cmd)
 
 
