b21e530a90c11aa9ccfefef9149d7172a3b039c0
Paul Craig <paul@pcraig3.ca>
2025-05-08 01:20:27 -0400

    Fix the tests after the migration
    There were a few little things here and there to update

--- bloom_nofos/nofos/views.py
+++ bloom_nofos/nofos/views.py
@@ -1,6 +1,5 @@
 import io
 import json
-import uuid
 from datetime import datetime
 
 import docraptor
@@ -109,7 +108,6 @@ def duplicate_nofo(original_nofo, is_successor=False):
         # Clone the NOFO
         new_nofo = Nofo.objects.get(pk=original_nofo.pk)
         new_nofo.id = None  # Clear the id to create a new instance
-        new_nofo.uuid = uuid.uuid4()  # new uuid
 
         if is_successor:
             # the "new" nofo is an archive, so it is the "succeeded" by the original one
