2b6b208a3686f1a867d6b8fa56e1c4f97ae3a2f2
Paul Craig <paul@pcraig3.ca>
2025-06-10 14:03:37 -0400

    Insource getting project version
    I've been using the django-project-version library to echo the
    version number from pyproject.toml into the <head> of my site.
    
    This has been working great, but now that this library is not
    updating its version requirements, I am not able to upgrade Django
    and get latest security releases.
    
    Since this package is ultimately doing something very simple,
    I decided I can just write a small function and pass the variable
    to my context processor and bing bang boom, we are no longer
    calling dj_version.

---
+++ nofos/bloom_nofos/version.py
@@ -0,0 +1,15 @@
+import os
+import tomli
+
+from django.conf import settings
+
+def get_version():
+    try:
+        base_dir = settings.BASE_DIR
+        pyproject_path = os.path.join(base_dir, "..", "pyproject.toml")
+
+        with open(pyproject_path, "rb") as f:
+            pyproject = tomli.load(f)
+            return pyproject["tool"]["poetry"]["version"]
+    except Exception:
+        return "unknown"
