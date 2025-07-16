5406cc390ba988a19a360de0136fadfac0437742
Paul Craig <paul@pcraig3.ca>
2025-07-16 18:41:35 -0400

    Create error state for bad SSO token
    Redid the alert include to make this work.
    
    We don't want all of our alerts to be on the bottom if they don't
    strictly need to be.

--- nofos/uploads/views.py
+++ nofos/uploads/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.conf import settings
+from django.contrib import messages
+from botocore.exceptions import TokenRetrievalError
 import boto3
 import re
 
@@ -13,18 +15,27 @@ class ImageListView(UserPassesTestMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        s3 = boto3.client("s3")
         bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-
-        response = s3.list_objects_v2(Bucket=bucket_name)
-        images = []
-
-        for obj in response.get("Contents", []):
-            key = obj["Key"]
-            if re.search(r"\.(jpe?g|png)$", key, re.IGNORECASE):
-                images.append(key)
-
-        context["images"] = images
+        context["images"] = []
         context["AWS_STORAGE_BUCKET_NAME"] = bucket_name
+
+        try:
+            s3 = boto3.client("s3")
+            response = s3.list_objects_v2(Bucket=bucket_name)
+
+            images = []
+            for obj in response.get("Contents", []):
+                key = obj["Key"]
+                if re.search(r"\.(jpe?g|png)$", key, re.IGNORECASE):
+                    images.append(key)
+
+            context["images"] = images
+
+        except TokenRetrievalError as e:
+            # Handle SSO token expiration gracefully
+            messages.error(
+                self.request,
+                "Your AWS SSO token has expired. Please run <code>aws sso login</code> in your terminal to refresh it.",
+            )
+
         return context
