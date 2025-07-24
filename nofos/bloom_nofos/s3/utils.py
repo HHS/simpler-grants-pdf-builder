1bb644a28dfb602a7e2a8fa0425bd0e3a9dc66cc
dfitchett <135860892+dfitchett@users.noreply.github.com>
2025-07-24 10:56:24 -0700

    Catch result of object not existing. Reject non-image objects.

--- nofos/bloom_nofos/s3/utils.py
+++ nofos/bloom_nofos/s3/utils.py
@@ -2,7 +2,7 @@ import logging
 
 import boto3
 from botocore.client import Config
-from botocore.exceptions import SSOTokenLoadError, TokenRetrievalError
+from botocore.exceptions import SSOTokenLoadError, TokenRetrievalError, ClientError
 from django.conf import settings
 
 logger = logging.getLogger("s3")
@@ -17,11 +17,15 @@ def get_image_url_from_s3(path):
 
     try:
         s3 = boto3.client("s3", config=Config(signature_version="s3v4"))
-        return s3.generate_presigned_url(
-            "get_object",
-            Params={"Bucket": bucket_name, "Key": path},
-            ExpiresIn=3600,
-        )
+        metadata = s3.head_object(Bucket=bucket_name, Key=path)
+        content_type = metadata.get("ContentType") if metadata else None
+        if content_type and content_type.startswith("image/"):
+            return s3.generate_presigned_url(
+                "get_object",
+                Params={"Bucket": bucket_name, "Key": path},
+                ExpiresIn=3600,
+            )
+        return None
 
     except TokenRetrievalError:
         logger.warning(
@@ -31,6 +35,11 @@ def get_image_url_from_s3(path):
     except SSOTokenLoadError:
         logger.warning(
             "No AWS SSO token found.",
+        )
+
+    except ClientError as e:
+        logger.warning(
+            f"An error occurred while accessing the AWS bucket: {e}",
         )
 
     except Exception as e:
