3dcd095dcafd3004408edf47f5f5b9cba5466b77
Paul Craig <paul@pcraig3.ca>
2025-07-28 15:45:13 -0400

    Allow <del> and <ins> in headings
    This is unfortunately an app-wide change, but it seems like it is
    not really a big deal.
    
    Similar to how we were previously allowing "br" elements into heading
    elements to show up as real <br> tags, we are doing the same for
    insert and delete tags so that they work in our compare view.
    
    Also, since we already have HTML coming back from the .diff, we
    don't need to be using safe_markdown. And, in fact, using safe_markdown
    was actually messing up our HTML. So we don't want it.

--- nofos/nofos/templatetags/safe_br.py
+++ nofos/nofos/templatetags/safe_br.py
@@ -36,3 +36,46 @@ def strip_br(value):
 
     # Just remove all <br>, <br/>, <br />, <BR>, etc.
     return BR_TAG_RE.sub("", value)
+
+
+@register.filter
+def safe_br_ins_del(value):
+    if not isinstance(value, str):
+        return value
+
+    # Define tag placeholders
+    placeholders = {
+        "<br>": "___BR___",
+        "<br/>": "___BR___",
+        "<br />": "___BR___",
+        "<ins>": "___INS_OPEN___",
+        "</ins>": "___INS_CLOSE___",
+        "<del>": "___DEL_OPEN___",
+        "</del>": "___DEL_CLOSE___",
+    }
+
+    # Regex to find all allowed tags, case-insensitive
+    TAG_RE = re.compile(r"</?(br\s*/?|ins|del)>", flags=re.IGNORECASE)
+
+    # Replace all allowed tags with placeholders
+    def replace_tag(match):
+        tag = match.group(0).lower().replace(" ", "")
+        if tag == "<br/>":
+            tag = "<br>"
+        return placeholders.get(tag, "")
+
+    replaced = TAG_RE.sub(replace_tag, value)
+
+    # Escape everything else
+    escaped = escape(replaced)
+
+    # Restore placeholders to real tags
+    restored = (
+        escaped.replace("___BR___", "<br>")
+        .replace("___INS_OPEN___", "<ins>")
+        .replace("___INS_CLOSE___", "</ins>")
+        .replace("___DEL_OPEN___", "<del>")
+        .replace("___DEL_CLOSE___", "</del>")
+    )
+
+    return mark_safe(restored)
