c022765b7d86d9f0245f8b92ce1851f53f1fdaa2
Paul Craig <paul@pcraig3.ca>
2025-07-28 11:37:17 -0400

    Create a compare page that looks more like a 'real' NOFO document
    For now, this is just for the "content guide" compare page.

--- nofos/guides/views.py
+++ nofos/guides/views.py
@@ -229,9 +229,7 @@ class ContentGuideCompareView(BaseNofoImportView):
             new_nofo.save()
 
             # Compare against the existing Content Guide
-            comparison = compare_nofos(
-                guide, new_nofo, statuses_to_ignore=["MATCH", "ADD"]
-            )
+            comparison = compare_nofos(guide, new_nofo)
 
             # Number of changes
             num_changed_sections = len(comparison)
