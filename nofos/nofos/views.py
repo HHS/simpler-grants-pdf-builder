60b4c327e476549cd0073d52fd985ed701c7faab
Paul Craig <paul@pcraig3.ca>
2025-08-21 17:34:55 -0400

    Show "Section admin link" in site header for "Configure section" page
    This is not really a huge priority, but it was a bug!
    
    Now it is not.
    
    Idk if this is useful in general, but a few days ago it would have been.

--- nofos/nofos/views.py
+++ nofos/nofos/views.py
@@ -1576,7 +1576,7 @@ class NofoSectionDetailView(GroupAccessObjectMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["subsection"] = self.object
+        context["section"] = self.object
         context["nofo"] = self.nofo
         return context
 
