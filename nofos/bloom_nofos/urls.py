a15eba1fa68f2b96c48698330a84933901b338a8
Paul Craig <paul@pcraig3.ca>
2025-10-13 18:01:56 -0400

    Remove the "guides" app completely from the code

--- nofos/bloom_nofos/urls.py
+++ nofos/bloom_nofos/urls.py
@@ -44,7 +44,6 @@ urlpatterns = [
     re_path(r"^favicon\.ico$", favicon_view),
     path("martor/", include("martor.urls")),
     path("nofos/", include("nofos.urls")),
-    path("guides/", include("guides.urls")),
     path("users/", include("users.urls")),
     path("uploads/", include("uploads.urls")),
     path("admin/", admin.site.urls),
