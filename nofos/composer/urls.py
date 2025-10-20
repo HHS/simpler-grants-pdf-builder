49b4ff3c2613577b52a4128a04f6b957cfda9102
Paul Craig <paul@pcraig3.ca>
2025-10-20 11:44:36 -0400

    Fix the index view, add "archive" view for Composer
    The index view will now show a table of existing content guides,
    and adds the "import" button.
    
    There is also now an "archive" view, so-called because users can't
    properly delete a guide, they can only archive it and it is archived for later.

--- nofos/composer/urls.py
+++ nofos/composer/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = "composer"
 
 urlpatterns = [
-    path("", views.WelcomeView.as_view(), name="composer_index"),
+    path("", views.ComposerListView.as_view(), name="composer_index"),
     path("import/", views.ComposerImportView.as_view(), name="composer_import"),
+    path(
+        "<uuid:pk>/delete", views.ComposerArchiveView.as_view(), name="composer_archive"
+    ),
 ]
--- nofos/composer/views.py
+++ nofos/composer/views.py
@@ -1,10 +1,14 @@
 from bloom_nofos.logs import log_exception
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import ListView, UpdateView
 
+from nofos.mixins import GroupAccessObjectMixinFactory
 from nofos.nofo import (
     add_headings_to_document,
     add_page_breaks_to_headings,
@@ -13,12 +17,30 @@ from nofos.nofo import (
 from nofos.utils import create_nofo_audit_event
 from nofos.views import BaseNofoImportView
 
-from .models import ContentGuideSection, ContentGuideSubsection
+from .models import ContentGuide, ContentGuideSection, ContentGuideSubsection
 from .utils import create_content_guide_document
 
+GroupAccessObjectMixin = GroupAccessObjectMixinFactory(ContentGuide)
 
-class WelcomeView(LoginRequiredMixin, TemplateView):
+
+class ComposerListView(LoginRequiredMixin, ListView):
+    model = ContentGuide
     template_name = "composer/composer_index.html"
+    context_object_name = "documents"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Exclude archived documents and documents that have a successor
+        queryset = queryset.filter(archived__isnull=True, successor__isnull=True)
+        # Return latest document first
+        queryset = queryset.order_by("-updated")
+
+        user_group = self.request.user.group
+        # If not a "bloom" user, return documents belonging to user's group
+        if user_group != "bloom":
+            queryset = queryset.filter(group=user_group)
+
+        return queryset
 
 
 class ComposerImportView(LoginRequiredMixin, BaseNofoImportView):
@@ -78,3 +100,30 @@ class ComposerImportView(LoginRequiredMixin, BaseNofoImportView):
                 status=500,
             )
             return HttpResponseBadRequest(f"Error creating Content Guide: {str(e)}")
+
+
+class ComposerArchiveView(GroupAccessObjectMixin, LoginRequiredMixin, UpdateView):
+    model = ContentGuide
+    template_name = "composer/composer_confirm_delete.html"
+    success_url = reverse_lazy("composer:composer_index")
+    context_object_name = "document"
+    fields = []  # We don’t need a form — just confirm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.archived:
+            return HttpResponseBadRequest("This document is already archived.")
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        document = self.get_object()
+        document.archived = timezone.now()
+        document.save(update_fields=["archived"])
+
+        messages.error(
+            request,
+            "You deleted “{}”.<br/>If this was a mistake, contact the NOFO Builder team at <a href='mailto:simplernofos@bloomworks.digital'>simplernofos@bloomworks.digital</a>.".format(
+                document.title
+            ),
+        )
+        return redirect(self.success_url)
