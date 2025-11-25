9ef44e153d87c61fdfd31b2782f5aa0d9ac5b442
Paul Craig <paul@pcraig3.ca>
2025-11-25 15:49:49 -0500

    Add an admin section for ContentGuideInstance
    Basically, we want to be able to administer this like we do for ContentGuide.

--- nofos/composer/admin.py
+++ nofos/composer/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from .models import ContentGuide, ContentGuideSection, ContentGuideSubsection
+from .models import (
+    ContentGuide,
+    ContentGuideInstance,
+    ContentGuideSection,
+    ContentGuideSubsection,
+)
 
 # --- Inlines -----------------------------------------------------------------
 
@@ -124,3 +129,89 @@ class ContentGuideAdmin(admin.ModelAdmin):
     @admin.action(description="Mark selected guides as Retired")
     def make_retired(self, request, queryset):
         queryset.update(status="retired")
+
+
+# --- ContentGuideInstance admin ------------------------------------------------------
+
+
+@admin.register(ContentGuideInstance)
+class ContentGuideInstanceAdmin(admin.ModelAdmin):
+    list_display = (
+        "title_or_short",
+        "parent_link",
+        "group",
+        "status",
+        "updated",
+        "archived",
+        "sections_count",
+    )
+    list_filter = ("group", "status")
+    search_fields = ("title", "short_name", "number", "agency")
+    ordering = ("-created",)
+    readonly_fields = (
+        "filename",
+        "created",
+        "updated",
+        "updated_by",
+    )
+
+    inlines = [ContentGuideSectionInline]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "short_name",
+                    "number",
+                    "agency",
+                    "group",
+                    "status",
+                    "archived",
+                    "filename",
+                    "parent",
+                    "created",
+                    "updated",
+                    "updated_by",
+                )
+            },
+        ),
+        (
+            "Conditional Questions",
+            {
+                "classes": ("collapse",),
+                "fields": ("conditional_questions",),
+            },
+        ),
+    )
+
+    #
+    # --- Computed helpers
+    #
+
+    def title_or_short(self, obj):
+        return obj.title or obj.short_name
+
+    title_or_short.short_description = "Title"
+
+    def sections_count(self, obj):
+        return obj.sections.count()
+
+    sections_count.short_description = "Sections"
+
+    def parent_link(self, obj):
+        if not obj.parent_id:
+            return "—"
+        url = reverse("admin:composer_contentguide_change", args=[obj.parent_id])
+        return format_html('<a href="{}">{}</a>', url, obj.parent.title)
+
+    parent_link.short_description = "Parent Guide"
+
+    #
+    # Record last editor
+    #
+    def save_model(self, request, obj, form, change):
+        if hasattr(obj, "updated_by"):
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
