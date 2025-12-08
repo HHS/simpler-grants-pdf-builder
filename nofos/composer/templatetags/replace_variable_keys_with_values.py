9c302eb6ac31307cda2d575e75b725dbafe2af0c
jlhogantech <jlhogan.tech@gmail.com>
2025-12-07 19:22:55 -0500

    Update because we're never actually changing the body

--- nofos/composer/templatetags/replace_variable_keys_with_values.py
+++ nofos/composer/templatetags/replace_variable_keys_with_values.py
@@ -12,20 +12,12 @@ def replace_variable_keys_with_values(html_string, variables_dict):
     # Find all span elements with class 'md-curly-variable'
     var_spans = soup.find_all("span", class_="md-curly-variable")
     for span in var_spans:
-        # check if the span has data-variable-key attribute, use that to match variable if exists
-        if "data-variable-key" in span.attrs:
-            var_key = span["data-variable-key"]
-            var_info = variables_dict.get(var_key, {})
-        # Otherwise, try to match by label inside the curly braces
-        else:
-            label = span.text.strip().strip("{}").strip()
-            var_key, var_info = find_variable_by_label(variables_dict, label)
+        # Look for a variable that matches the label
+        label = span.text.strip().strip("{}").strip()
+        var_key, var_info = find_variable_by_label(variables_dict, label)
 
         # If a matching variable exists, replace the content
         if var_key:
-            # Add the variable key as a data attribute for future updates
-            span["data-variable-key"] = var_key
-
             var_value = var_info.get("value", None)
             if var_value:
                 # Update the span with the variable value
