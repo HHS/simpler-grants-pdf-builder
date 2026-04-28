e37fcac48e36ca488138a92fe4a99b52ffe21600
Paul Craig <paul@pcraig3.ca>
2026-04-28 10:52:07 -0400

    Refactor user tests
    This saves us a bunch of setup work and reduces some lines of code
    in the user testing file.

--- nofos/users/tests_users/test_team.py
+++ nofos/users/tests_users/test_team.py
@@ -3,10 +3,8 @@ from django.urls import reverse
 from users.models import BloomUser
 
 
-class BloomTeamViewTests(TestCase):
+class BloomUserTeamBaseTests(TestCase):
     def setUp(self):
-        self.url = reverse("users:user_team")
-
         self.superuser = BloomUser.objects.create_user(
             email="admin@example.com",
             password="testpass123",
@@ -14,6 +12,24 @@ class BloomTeamViewTests(TestCase):
             group="bloom",
             is_staff=True,
             is_superuser=True,
+            force_password_reset=False,
+        )
+
+        self.other_superuser = BloomUser.objects.create_user(
+            email="other-admin@example.com",
+            password="testpass123",
+            full_name="Other Admin",
+            group="bloom",
+            is_staff=True,
+            is_superuser=True,
+            force_password_reset=False,
+        )
+
+        self.bloom_user = BloomUser.objects.create_user(
+            email="bloom-user@example.com",
+            password="testpass123",
+            full_name="Bloom User",
+            group="bloom",
             force_password_reset=False,
         )
 
@@ -25,17 +41,26 @@ class BloomTeamViewTests(TestCase):
             force_password_reset=False,
         )
 
-    def test_anonymous_user_gets_403(self):
-        response = self.client.get(self.url)
+    def assert_superuser_required(self, url):
+        self.client.logout()
 
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
 
-    def test_non_superuser_gets_403(self):
         self.client.force_login(self.regular_user)
-
-        response = self.client.get(self.url)
-
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
+
+        self.client.logout()
+
+
+class BloomTeamViewTests(BloomUserTeamBaseTests):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("users:user_team")
+
+    def test_superuser_required(self):
+        self.assert_superuser_required(self.url)
 
     def test_superuser_can_view_team_page(self):
         self.client.force_login(self.superuser)
@@ -44,14 +69,13 @@ class BloomTeamViewTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "All users")
-        self.assertContains(response, "admin@example.com")
         self.assertContains(response, "user@example.com")
         self.assertContains(response, "Regular User")
         self.assertContains(response, "ACF: Administration for Children and Families")
 
         # the current user is not visible in the "team" table
+        self.assertNotContains(response, 'data-sort-value="admin@example.com"')
         self.assertNotContains(response, "Admin User")
-        self.assertNotContains(response, "Bloomworks")
 
     def test_team_page_uses_uswds_sortable_columns_for_email_and_group(self):
         self.client.force_login(self.superuser)
@@ -72,39 +96,13 @@ class BloomTeamViewTests(TestCase):
         self.assertNotContains(response, '<th data-sortable scope="col">Superuser</th>')
 
 
-class BloomUserTeamCreateViewTests(TestCase):
+class BloomUserTeamCreateViewTests(BloomUserTeamBaseTests):
     def setUp(self):
+        super().setUp()
         self.url = reverse("users:user_team_create")
 
-        self.superuser = BloomUser.objects.create_user(
-            email="admin@example.com",
-            password="testpass123",
-            full_name="Admin User",
-            group="bloom",
-            is_staff=True,
-            is_superuser=True,
-            force_password_reset=False,
-        )
-
-        self.regular_user = BloomUser.objects.create_user(
-            email="user@example.com",
-            password="testpass123",
-            full_name="Regular User",
-            group="acf",
-            force_password_reset=False,
-        )
-
-    def test_anonymous_user_gets_403(self):
-        response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 403)
-
-    def test_non_superuser_gets_403(self):
-        self.client.force_login(self.regular_user)
-
-        response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 403)
+    def test_superuser_required(self):
+        self.assert_superuser_required(self.url)
 
     def test_superuser_can_view_create_user_page(self):
         self.client.force_login(self.superuser)
@@ -193,49 +191,13 @@ class BloomUserTeamCreateViewTests(TestCase):
         )
 
 
-class BloomUserTeamDeleteViewTests(TestCase):
+class BloomUserTeamDeleteViewTests(BloomUserTeamBaseTests):
     def setUp(self):
-        self.superuser = BloomUser.objects.create_user(
-            email="admin@example.com",
-            password="testpass123",
-            full_name="Admin User",
-            group="bloom",
-            is_staff=True,
-            is_superuser=True,
-            force_password_reset=False,
-        )
-
-        self.other_superuser = BloomUser.objects.create_user(
-            email="other-admin@example.com",
-            password="testpass123",
-            full_name="Other Admin",
-            group="bloom",
-            is_staff=True,
-            is_superuser=True,
-            force_password_reset=False,
-        )
-
-        self.regular_user = BloomUser.objects.create_user(
-            email="user@example.com",
-            password="testpass123",
-            full_name="Regular User",
-            group="acf",
-            force_password_reset=False,
-        )
-
+        super().setUp()
         self.url = reverse("users:user_team_delete", args=[self.regular_user.pk])
 
-    def test_anonymous_user_gets_403(self):
-        response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 403)
-
-    def test_non_superuser_gets_403(self):
-        self.client.force_login(self.regular_user)
-
-        response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 403)
+    def test_superuser_required(self):
+        self.assert_superuser_required(self.url)
 
     def test_superuser_can_view_delete_confirmation_page(self):
         self.client.force_login(self.superuser)
@@ -279,3 +241,265 @@ class BloomUserTeamDeleteViewTests(TestCase):
 
         self.assertRedirects(response, reverse("users:user_team"))
         self.assertTrue(BloomUser.objects.filter(email="admin@example.com").exists())
+
+
+class BloomUserTeamDetailViewTests(BloomUserTeamBaseTests):
+    def test_superuser_required(self):
+        url = reverse("users:user_team_detail", args=[self.regular_user.pk])
+
+        self.assert_superuser_required(url)
+
+    def test_superuser_can_view_user_detail_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_detail", args=[self.regular_user.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "user@example.com")
+        self.assertContains(response, "Regular User")
+        self.assertContains(response, "ACF: Administration for Children and Families")
+        self.assertContains(response, "Change group")
+        self.assertContains(response, "Reset password")
+
+    def test_superuser_row_only_shows_for_bloom_users(self):
+        self.client.force_login(self.superuser)
+
+        bloom_response = self.client.get(
+            reverse("users:user_team_detail", args=[self.bloom_user.pk])
+        )
+        regular_response = self.client.get(
+            reverse("users:user_team_detail", args=[self.regular_user.pk])
+        )
+
+        self.assertContains(bloom_response, "Is Superuser")
+        self.assertContains(bloom_response, "Change Superuser status")
+        self.assertNotContains(regular_response, "Is Superuser")
+        self.assertNotContains(regular_response, "Change Superuser status")
+
+
+class BloomUserTeamNameEditViewTests(BloomUserTeamBaseTests):
+    def test_superuser_required(self):
+        url = reverse("users:user_team_edit_name", args=[self.regular_user.pk])
+
+        self.assert_superuser_required(url)
+
+    def test_superuser_can_view_name_edit_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_edit_name", args=[self.regular_user.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Change user name")
+        self.assertContains(response, "Full name")
+
+    def test_superuser_can_change_user_name(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_name", args=[self.regular_user.pk]),
+            {
+                "full_name": "Updated User",
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.regular_user.pk]),
+        )
+
+        self.regular_user.refresh_from_db()
+        self.assertEqual(self.regular_user.full_name, "Updated User")
+
+
+class BloomUserTeamGroupEditViewTests(BloomUserTeamBaseTests):
+    def test_superuser_required(self):
+        url = reverse("users:user_team_edit_group", args=[self.regular_user.pk])
+
+        self.assert_superuser_required(url)
+
+    def test_superuser_can_view_group_edit_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_edit_group", args=[self.regular_user.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Change user group")
+        self.assertContains(response, "Group")
+
+    def test_superuser_can_change_regular_user_group(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_group", args=[self.regular_user.pk]),
+            {
+                "group": "cdc",
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.regular_user.pk]),
+        )
+
+        self.regular_user.refresh_from_db()
+        self.assertEqual(self.regular_user.group, "cdc")
+
+    def test_cannot_change_superuser_to_non_bloom_group(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_group", args=[self.other_superuser.pk]),
+            {
+                "group": "acf",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "Remove Superuser status before changing this user to a non-Bloom group.",
+        )
+
+        self.other_superuser.refresh_from_db()
+        self.assertEqual(self.other_superuser.group, "bloom")
+        self.assertTrue(self.other_superuser.is_superuser)
+        self.assertTrue(self.other_superuser.is_staff)
+
+
+class BloomUserTeamSuperuserEditViewTests(BloomUserTeamBaseTests):
+    def test_superuser_required(self):
+        url = reverse("users:user_team_edit_superuser", args=[self.bloom_user.pk])
+
+        self.assert_superuser_required(url)
+
+    def test_superuser_can_view_superuser_edit_page_for_bloom_user(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_edit_superuser", args=[self.bloom_user.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Change Superuser status")
+        self.assertContains(response, "Is Superuser")
+
+    def test_non_bloom_user_cannot_access_superuser_edit_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_edit_superuser", args=[self.regular_user.pk])
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.regular_user.pk]),
+        )
+
+    def test_superuser_can_make_bloom_user_a_superuser(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_superuser", args=[self.bloom_user.pk]),
+            {
+                "is_superuser": "on",
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.bloom_user.pk]),
+        )
+
+        self.bloom_user.refresh_from_db()
+        self.assertTrue(self.bloom_user.is_superuser)
+        self.assertTrue(self.bloom_user.is_staff)
+
+    def test_superuser_can_remove_superuser_status_from_another_user(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_superuser", args=[self.other_superuser.pk]),
+            {},
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.other_superuser.pk]),
+        )
+
+        self.other_superuser.refresh_from_db()
+        self.assertFalse(self.other_superuser.is_superuser)
+        self.assertFalse(self.other_superuser.is_staff)
+
+    def test_superuser_cannot_remove_superuser_status_from_self(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_edit_superuser", args=[self.superuser.pk]),
+            {},
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.superuser.pk]),
+        )
+
+        self.superuser.refresh_from_db()
+        self.assertTrue(self.superuser.is_superuser)
+        self.assertTrue(self.superuser.is_staff)
+
+
+class BloomUserTeamPasswordResetViewTests(BloomUserTeamBaseTests):
+    def test_superuser_required(self):
+        url = reverse("users:user_team_reset_password", args=[self.bloom_user.pk])
+
+        self.assert_superuser_required(url)
+
+    def test_superuser_can_view_password_reset_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_reset_password", args=[self.regular_user.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Reset user password")
+        self.assertContains(response, "user@example.com")
+
+    def test_superuser_can_reset_another_users_password(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.post(
+            reverse("users:user_team_reset_password", args=[self.regular_user.pk]),
+            {
+                "new_password1": "a-very-good-password-12345",
+                "new_password2": "a-very-good-password-12345",
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.regular_user.pk]),
+        )
+
+        self.regular_user.refresh_from_db()
+        self.assertTrue(self.regular_user.check_password("a-very-good-password-12345"))
+        self.assertTrue(self.regular_user.force_password_reset)
+
+    def test_superuser_cannot_reset_own_password_from_team_page(self):
+        self.client.force_login(self.superuser)
+
+        response = self.client.get(
+            reverse("users:user_team_reset_password", args=[self.superuser.pk])
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("users:user_team_detail", args=[self.superuser.pk]),
+        )
