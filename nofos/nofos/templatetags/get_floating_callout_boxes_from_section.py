24fdb15af229e8fd189761c2f113f3ec1f02ad9e
mikec-ai <mike.collier@agile6.com>
2026-07-23 10:40:31 -0400

    Add dense callout font tier

--- nofos/nofos/templatetags/get_floating_callout_boxes_from_section.py
+++ nofos/nofos/templatetags/get_floating_callout_boxes_from_section.py
@@ -21,3 +21,17 @@ def get_combined_wordcount_for_subsections(subsections):
         word_count += len(subsection.body.split())
 
     return word_count
+
+
+@register.filter()
+def get_floating_callout_size_classes(subsections):
+    word_count = get_combined_wordcount_for_subsections(subsections)
+
+    if word_count > 149:
+        return "section--content--right-col--tiny section--content--right-col--dense"
+    if word_count > 99:
+        return "section--content--right-col--tiny"
+    if word_count > 79:
+        return "section--content--right-col--smaller"
+
+    return ""
